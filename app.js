2f46db84b313c4f58760acd0d07f82ea88dbb78c
RDAxRoadkill <wouter97@planet.nl>
2016-09-04 23:54:54 +0200

    Eerste setup website

---
+++ app.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+//Sesions
+var session = require('express-session');
+var memcached = require('connect-memcached')(session);
+var cookieStore = new memcached({host: "localhost:8000"});
+var flash = require('connect-flash');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+//configure
+app.set('views', path.join(__dirname + '/public/views' ));
+app.set('view engine', 'twig');
+
+
+//middleware
+app.use(session({secret: '123', cookie: {maxAge: 60000}, resave: false, saveUninitialized: true}));
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(flash());
+app.use(express.static(path.join(__dirname,'/public')));
+app.use('/bower_components', express.static(path.join(__dirname,'/bower_components')));
+
+
+app.use(require('./routes'));
+
+app.listen(port, function(){
+    console.log('----======<<< STARTED ON PORT: ' + port + ' >>>======----');
+});
+
+module.exports = app;
+
+
+/* default // old
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;*/ 
