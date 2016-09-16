d954ec140d79dfdfc091255123b3e5680a52fc85
RDAxRoadkill <wouter97@planet.nl>
2016-09-16 13:04:27 +0200

    Update 16 September 2016

--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var sess;
-var prijs = null;
 
 //Includes
 
@@ -12,8 +10,18 @@ router.get('/', function (req, res) {
     res.render('partials/home.html.twig');
 });
 
+//Non-function Routes
 router.get('/contact', function(req,res){
    res.render('partials/contact.html.twig'); 
+});
+router.get('/tickets', function(req,res){
+   res.render('partials/tickets.html.twig'); 
+});
+router.get('/inschrijvenSpreker', function(req,res){
+   res.render('partials/spreker.html.twig'); 
+});
+router.get('/tijdslot', function(req,res){
+   res.render('partials/tijdslot.html.twig'); 
 });
 
 module.exports = router;
